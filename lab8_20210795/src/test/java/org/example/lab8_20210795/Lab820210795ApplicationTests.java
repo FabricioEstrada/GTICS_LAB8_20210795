4f5e7ab124c780a5fb75790545997775cace881b
FabricioEstrada <a20210795@pucp.edu.pe>
2024-11-01 00:45:54 -0500

    olaa

---
+++ lab8_20210795/src/test/java/org/example/lab8_20210795/Lab820210795ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.lab8_20210795;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab820210795ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
